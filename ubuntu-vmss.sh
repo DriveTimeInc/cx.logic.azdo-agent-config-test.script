b408a0b19907489b3433b7f242054a718a9604e1
dandrivetime <dan.anderson@drivetime.com>
2024-07-26 20:13:51 -0400

    try alternate install method

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -29,7 +29,8 @@ sudo apt-get install -y \
     liblzma-dev \
     libxml2-dev \
     libxmlsec1-dev \
-    libbz2-dev
+    libbz2-dev \
+    python3.11-venv
 
 
 # Download the Microsoft repository GPG keys
@@ -70,33 +71,71 @@ curl -LO "https://github.com/argoproj/argo-rollouts/releases/latest/download/kub
 chmod +x ./kubectl-argo-rollouts-linux-amd64
 mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
-# install pyenv
-mkdir -p /home/AzDevOps
-git clone https://github.com/pyenv/pyenv.git /home/AzDevOps/.pyenv
+# # install pyenv
+# mkdir -p /home/AzDevOps
+# git clone https://github.com/pyenv/pyenv.git /home/AzDevOps/.pyenv
 
-# set env variables for provisioning
-export PYENV_ROOT="/home/AzDevOps/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+# # set env variables for provisioning
+# export PYENV_ROOT="/home/AzDevOps/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
 
-# load pyenv into the shell
-eval "$(pyenv init -)"
+# # load pyenv into the shell
+# eval "$(pyenv init -)"
 
-# install 3.11.9
-pyenv install 3.11.9
+# # install 3.11.9
+# pyenv install 3.11.9
 
-# set the system to use 3.11.9
-pyenv global 3.11.9
+# # set the system to use 3.11.9
+# pyenv global 3.11.9
 
-# give all perms to the .pyenv folder
-chmod -R a+rwx /home/AzDevOps/.pyenv
+# # give all perms to the .pyenv folder
+# chmod -R a+rwx /home/AzDevOps/.pyenv
 
-# setup pyenv entries in .bashrc
-echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.bashrc
-echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.bashrc
+# # setup pyenv entries in .bashrc
+# echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.bashrc
+# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.bashrc
+# echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.bashrc
 
-# setup pyenv entries in .profile
-echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.profile
-echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.profile
+# # setup pyenv entries in .profile
+# echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.profile
+# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.profile
+# echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.profile
+
+# from: https://learn.microsoft.com/en-us/azure/devops/pipelines/tasks/reference/use-python-version-v0?view=azure-pipelines#how-can-i-configure-a-self-hosted-agent-to-use-this-task
+LOCAL_AGENT_TOOLS_DIR="/agent/_work/_tool"
+LOCAL_PYTHON_MAJOR_VERSION="3"
+LOCAL_PYTHON_MINOR_VERSION="11"
+LOCAL_PYTHON_PATCH_VERSION="9"
+
+
+LOCAL_PYTHON_MAJOR_MINOR_DIR="$LOCAL_AGENT_TOOLS_DIR/Python/$LOCAL_PYTHON_MAJOR_VERSION.$LOCAL_PYTHON_MINOR_VERSION"
+LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR="$LOCAL_AGENT_TOOLS_DIR/Python/$LOCAL_PYTHON_MAJOR_VERSION.$LOCAL_PYTHON_MINOR_VERSION.$LOCAL_PYTHON_PATCH_VERSION"
+
+# Create the directory for 3.11
+mkdir -p "$LOCAL_PYTHON_MAJOR_MINOR_DIR"
+
+# Create the directory for 3.11.9
+mkdir -p "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR"
+
+# Create the simlink from 3.11, to 3.11.9
+# this is so the tool installer task can specify 3.11, or 3.11.9
+ln -s "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR" "$LOCAL_PYTHON_MAJOR_MINOR_DIR"
+
+# create the venv inside the 3.11.9/x64 directory
+python3.11 -m venv "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR/x64"
+
+# create the .complete file
+touch "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR/x64.complete"
+
+
+# # structure must match the following:
+# $AGENT_TOOLSDIRECTORY/
+#     Python/
+#         3.11.9/
+#             x64/
+#                 {tool files}
+#             x64.complete
+
+
+
 
