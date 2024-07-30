8d1d2d01f825c2277e45f0f0a3ff935f52990308
dandrivetime <dan.anderson@drivetime.com>
2024-07-30 10:45:19 -0400

    try pyenv with tool installer

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -29,9 +29,10 @@ sudo apt-get install -y \
     liblzma-dev \
     libxml2-dev \
     libxmlsec1-dev \
-    libbz2-dev \
-    python3.11-venv
+    libbz2-dev
+    
 
+#python3.11-venv
 
 # Download the Microsoft repository GPG keys
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
@@ -71,71 +72,50 @@ curl -LO "https://github.com/argoproj/argo-rollouts/releases/latest/download/kub
 chmod +x ./kubectl-argo-rollouts-linux-amd64
 mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
-# # install pyenv
-# mkdir -p /home/AzDevOps
-# git clone https://github.com/pyenv/pyenv.git /home/AzDevOps/.pyenv
+# install pyenv
+git clone https://github.com/pyenv/pyenv.git /usr/local/bin/.pyenv
 
-# # set env variables for provisioning
-# export PYENV_ROOT="/home/AzDevOps/.pyenv"
-# export PATH="$PYENV_ROOT/bin:$PATH"
+# set env variables for provisioning
+export PYENV_ROOT="/usr/local/bin/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
-# # load pyenv into the shell
-# eval "$(pyenv init -)"
+# load pyenv into the shell
+eval "$(pyenv init -)"
 
-# # install 3.11.9
-# pyenv install 3.11.9
+# install 3.11.9
+pyenv install 3.11.9
 
-# # set the system to use 3.11.9
-# pyenv global 3.11.9
-
-# # give all perms to the .pyenv folder
-# chmod -R a+rwx /home/AzDevOps/.pyenv
-
-# # setup pyenv entries in .bashrc
-# echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.bashrc
-# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.bashrc
-# echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.bashrc
-
-# # setup pyenv entries in .profile
-# echo 'export PYENV_ROOT="/home/AzDevOps/.pyenv"' >> /home/AzDevOps/.profile
-# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/AzDevOps/.profile
-# echo 'eval "$(pyenv init -)"' >> /home/AzDevOps/.profile
+# set the system to use 3.11.9
+pyenv global 3.11.9
 
 # from: https://learn.microsoft.com/en-us/azure/devops/pipelines/tasks/reference/use-python-version-v0?view=azure-pipelines#how-can-i-configure-a-self-hosted-agent-to-use-this-task
+# also from: https://dev.to/akaszynski/create-an-azure-self-hosted-agent-without-going-insane-173g
 LOCAL_AGENT_TOOLS_DIR="/agent/_work/_tool"
-LOCAL_PYTHON_MAJOR_VERSION="3"
-LOCAL_PYTHON_MINOR_VERSION="11"
-LOCAL_PYTHON_PATCH_VERSION="9"
-
-
-LOCAL_PYTHON_MAJOR_MINOR_DIR="$LOCAL_AGENT_TOOLS_DIR/Python/$LOCAL_PYTHON_MAJOR_VERSION.$LOCAL_PYTHON_MINOR_VERSION"
-LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR="$LOCAL_AGENT_TOOLS_DIR/Python/$LOCAL_PYTHON_MAJOR_VERSION.$LOCAL_PYTHON_MINOR_VERSION.$LOCAL_PYTHON_PATCH_VERSION"
-
-# Create the directory for 3.11
-mkdir -p "$LOCAL_PYTHON_MAJOR_MINOR_DIR"
-
-# Create the directory for 3.11.9
-mkdir -p "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR"
-
-# Create the simlink from 3.11, to 3.11.9
-# this is so the tool installer task can specify 3.11, or 3.11.9
-ln -s "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR" "$LOCAL_PYTHON_MAJOR_MINOR_DIR"
-
-# create the venv inside the 3.11.9/x64 directory
-python3.11 -m venv "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR/x64"
-
-# create the .complete file
-touch "$LOCAL_PYTHON_MAJOR_MINOR_PATCH_DIR/x64.complete"
-
 
 # # structure must match the following:
 # $AGENT_TOOLSDIRECTORY/
 #     Python/
+#         3.11/ -> 3.11.9/
 #         3.11.9/
 #             x64/
 #                 {tool files}
 #             x64.complete
 
+# Create the directory for 3.11
+mkdir -p "$LOCAL_AGENT_TOOLS_DIR/Python/3.11"
 
+# Create the directory for 3.11.9
+mkdir -p "$LOCAL_AGENT_TOOLS_DIR/Python/3.11.9"
 
+# Create the simlink from 3.11, to 3.11.9
+# this is so the tool installer task can specify 3.11, or 3.11.9
+ln -s "$LOCAL_AGENT_TOOLS_DIR/Python/3.11.9" "$LOCAL_AGENT_TOOLS_DIR/Python/3.11"
+
+# create the venv inside the 3.11.9/x64 directory
+# the python3.11 command is from the python3.11-venv apt package
+# python3.11 -m venv "$LOCAL_AGENT_TOOLS_DIR/Python/3.11.9/x64"
+python -m venv "$LOCAL_AGENT_TOOLS_DIR/Python/3.11.9/x64"
+
+# create the .complete file
+touch "$LOCAL_AGENT_TOOLS_DIR/Python/3.11.9/x64.complete"
 
